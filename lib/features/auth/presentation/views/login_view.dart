a750acf977de114a694a80026f4bdaebf13cefee
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-06-20 13:32:13 +0300

    [ADD] finish auth logic

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,6 +1,7 @@
-
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:scholariship/features/auth/presentation/widgets/login_form.dart';
+import 'package:scholariship/features/auth/presentation/widgets/register_form.dart';
 
 @RoutePage()
 class LoginScreen extends StatelessWidget {
@@ -8,11 +9,71 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              weight: 2,
+            ),
+            onPressed: () {},
+          ),
+          title: Text(
+            '',
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge
+                ?.copyWith( fontSize: 22),
+          ),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(22.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Login',
+                      style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    const LoginForm(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Already have an account ?",
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                context.router.replaceNamed("/register");
+                              },
+                              child: Text(
+                                "sign up here",
+                                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                    decoration: TextDecoration.underline
+                                ),
+                              ),)
+                          ],
+                        )
+                      ],
+                    )
 
-      body: ElevatedButton(onPressed: (){
-        context.router.replaceNamed("/navigation");
-      }, child: const Text("Login")),
-    );
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/global/widgets/custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final Function onPressed;
+
+  const CustomButton({super.key, required this.label, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          onPressed();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue, // Background color
+          foregroundColor: Colors.white, // Text color
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0), // Rounded corners
+          ),
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+
+        ),
+        child: Text(label),
+      ),
+    );
+  }
+}
