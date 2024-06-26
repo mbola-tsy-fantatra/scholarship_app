89a35b16fb682e0a7fad657cca1ee65309a3968f
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-06-26 21:58:22 +0300

    [FIX] start date null error

--- lib/features/scholariship/domain/entities/sholarship.dart
+++ lib/features/scholariship/domain/entities/sholarship.dart
@@ -15,7 +15,7 @@ class Scholarship extends Equatable{
   final String coverPhoto;
   final String applicationStartPeriod;
   final String fundingType;
-  final List<ProfileSaved>? savedBy;
+  final List<dynamic>? savedBy;
 
   const Scholarship({required this.fundingType,required this.applicationStartPeriod,required this.description,required this.id, required this.name, required this.applicationStartDate, required this.applicationEndDate, required this.officialLink, required this.organizationName, required this.coverPhoto,this.savedBy});
 
